c21c8be74c7617033e6902a361bc132b6115bc7f
Tom Lou <louyu27@cs.washington.edu>
2020-01-05 21:14:27 -0800

    Simplify normalization

--- prepare_images.py
+++ prepare_images.py
@@ -15,24 +15,13 @@ def normalize_and_rgb(images):
 
     Normalize the image and covert to 3 channels by duplication
     '''
-    import numpy as np
     # normalize image to 0-255 per image.
-    image_sum = 1 / np.sum(np.sum(images, axis=1), axis=-1)
-    given_axis = 0
-    # Create an array which would be used to reshape 1D array, b to have
-    # singleton dimensions except for the given axis where we would put -1
-    # signifying to use the entire length of elements along that axis
-    dim_array = np.ones((1, images.ndim), int).ravel()
-    dim_array[given_axis] = -1
-    # Reshape b with dim_array and perform elementwise multiplication with
-    # broadcasting along the singleton dimensions for the final output
-    image_sum_reshaped = image_sum.reshape(dim_array)
-    images = images * image_sum_reshaped * 255
+    img /= np.sum(img)
 
     # make it rgb by duplicating 3 channels.
-    images = np.stack([images, images, images], axis=-1)
+    img = np.stack([img, img, img], axis=-1)
 
-    return images
+    return img
 
 
 def read_images(fileList):
